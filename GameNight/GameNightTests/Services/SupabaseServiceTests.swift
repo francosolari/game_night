c4cd0bd9e23e0114a3b98cf35e89ab4bfda0e2af
francosolari <fsolari@comcast.net>
2026-04-04 12:15:59 -0400

    test: add @MainActor to SupabaseServiceTests method for UI safety

--- GameNight/GameNightTests/Services/SupabaseServiceTests.swift
+++ GameNight/GameNightTests/Services/SupabaseServiceTests.swift
@@ -2,6 +2,7 @@ import XCTest
 @testable import GameNight
 
 final class SupabaseServiceTests: XCTestCase {
+    @MainActor
     func testEventSelectContainsRequiredRelations() {
         let select = SupabaseService.eventSelect
 
